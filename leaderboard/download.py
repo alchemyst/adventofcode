dd29d3670c76c42e4863a7c56bd8818f891dac02
Carl Sandrock <carl.sandrock@proquoai.com>
2021-12-10 13:21:49 +0200

    Automatically download leaderboard

---
+++ leaderboard/download.py
@@ -0,0 +1,23 @@
+import datetime
+import pathlib
+import sys
+
+import requests
+import aocd
+
+LEADERBOARD_ID = "1189810"
+
+URL = f"https://adventofcode.com/2021/leaderboard/private/view/{LEADERBOARD_ID}.json"
+
+target = pathlib.Path(f'{LEADERBOARD_ID}.json')
+
+if target.exists() and modified(target) < datetime.timedelta(minutes=15):
+    sys.exit()
+
+# Get user token
+user = aocd.get.default_user()
+token = user.token
+
+data = requests.get(URL, cookies={'session': token})
+
+target.write_bytes(data.content)
