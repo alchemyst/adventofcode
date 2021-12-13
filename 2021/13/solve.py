790bc937e9fdf7feb242e49b8898591ad7d5449f
Carl Sandrock <carl.sandrock@proquoai.com>
2021-12-13 08:15:38 +0200

    Day 13

---
+++ 2021/13/solve.py
@@ -0,0 +1,84 @@
+import numpy as np
+import re
+from aoc import print_board
+
+chars = {0: '.', 1: '#'}
+def p(board):
+    print_board(board, lookup=chars, type='s')
+
+foldre = re.compile(r'fold along ([xy])=(\d+)')
+
+debug = False
+filename = 'test.txt' if debug else 'input.txt'
+
+xs = []
+ys = []
+folds = []
+with open(filename) as f:
+    for line in f:
+        line = line.strip()
+        if not line:
+            continue
+        if ',' in line:
+            x, y = map(int, line.split(','))
+            xs.append(x)
+            ys.append(y)
+        if 'fold' in line:
+            m = foldre.match(line)
+            direction, pos = m.groups()
+            pos = int(pos)
+            folds.append([direction, pos])
+
+board = np.zeros((max(ys)+1, max(xs)+1), dtype=int)
+board[ys, xs] = 1
+
+
+def foldud(board, position):
+    top, bottom = board[0:position], board[position+1:]
+    width = board.shape[1]
+    topheight = top.shape[0]
+    bottomheight = bottom.shape[0]
+    if debug:
+        print("Proposed fold:")
+        p(top)
+        print('-'*width)
+        p(bottom)
+    height = max(topheight, bottomheight)
+    newboard = np.zeros((height, width), dtype=int)
+    newboard[-topheight:] = top
+    newboard[-bottomheight:] |= np.flipud(bottom)
+    if debug:
+        print("New board:")
+        p(newboard)
+        print()
+    return newboard
+
+
+def rot(board):
+    return np.rot90(board, k=3)
+
+
+def dofold(board, direction, position):
+    if direction == 'y':
+        return foldud(board, position)
+    else:
+        return foldud(board.T, position).T
+
+
+# Part 1
+if debug:
+    print('Board')
+    p(board)
+direction, position = folds[0]
+newboard = dofold(board, direction, position)
+if debug:
+    print()
+    p(newboard)
+print('Part 1:', newboard.sum())
+
+# Part 2
+for direction, position in folds[1:]:
+    newboard = dofold(newboard, direction, position)
+
+print("Part 2:")
+p(newboard)
--- aoc/display.py
+++ aoc/display.py
@@ -2,12 +2,16 @@ import rich
 import rich.text
 import numpy as np
 
-def print_board(array, highlight=None, length=1):
+def print_board(array, highlight=None, length=1, lookup=None, type='d'):
     if highlight is None:
         highlight = np.zeros_like(array, dtype=bool)
     for i, row in enumerate(array):
         for j, value in enumerate(row):
-            text = rich.text.Text(f'{value:{length}d}')
+            if lookup:
+                v = lookup[value]
+            else:
+                v = value
+            text = rich.text.Text(f'{v:{length}{type}}')
             if highlight[i, j]:
                 text.stylize(style='bold magenta')
             rich.print(text, end='')
