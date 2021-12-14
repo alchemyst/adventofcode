fad05155ec30d919f99cc82597b3f2bbea57fe79
Carl Sandrock <carl.sandrock@proquoai.com>
2021-12-14 10:29:37 +0200

    Streamline day 14

--- 2021/14/solve.py
+++ 2021/14/solve.py
@@ -4,28 +4,12 @@ debug = False
 filename = 'test.txt' if debug else 'input.txt'
 
 
-def pairwise(iterable):
-    return zip(iterable, iterable[1:])
-
 def parse(filename):
     with open(filename) as f:
         template = next(f).strip()
         next(f)
         rules = dict(line.strip().split(' -> ') for line in f)
     return template, rules
-
-# I implemented the basic string-based rules first,
-# then used that to debug the pairwise implementation
-
-def polymerize(polymer):
-    newpolymer = []
-    for a, b in pairwise(polymer):
-        s = a+b
-        newpolymer.append(a)
-        if s in rules:
-            newpolymer.append(rules[s])
-    newpolymer.append(b)
-    return ''.join(newpolymer)
 
 
 def pair_polymerize(pairs):
@@ -41,38 +25,25 @@ def pair_polymerize(pairs):
     return newpairs
 
 
+def solve(steps):
+    # polymerize
+    pairs = Counter(zip(template, template[1:]))
+    for i in range(steps):
+        pairs = pair_polymerize(pairs)
+
+    # Count
+    counts = Counter()
+    for (a, _), count in pairs.items():
+        counts[a] += count
+    # Tricky - you have to add the last item,
+    # which will never change because you're always adding in the middle of two
+    counts[template[-1]] += 1
+
+    return max(counts.values()) - min(counts.values())
+
+
 template, rules = parse(filename)
-
-polymer = template
-# print("Template:        ", polymer)
-for i in range(10):
-    step = i + 1
-    polymer = polymerize(polymer)
-    # print(F"After step {step}: ", polymer)
-
-count = Counter(polymer)
-if debug:
-    print(count)
-print('Part 1:', max(count.values()) - min(count.values()))
-
-
-# Part 2
-
-# Clearly we need to be more efficient in storing thes things - just store pairs
-pairs = Counter(pairwise(template))
 pair_rules = {tuple(rule): value for rule, value in rules.items()}
 
-# Polymerize
-newpairs = pairs.copy()
-for i in range(40):
-    newpairs = pair_polymerize(newpairs)
-
-# Count
-newcounts = Counter()
-for (a, _), count in newpairs.items():
-    newcounts[a] += count
-# Tricky - you have to add the last item,
-# which will never change because you're always adding in the middle of two
-newcounts[template[-1]] += 1
-
-print('Part 2:', max(newcounts.values()) - min(newcounts.values()))
+print('Part 1:', solve(10))
+print('Part 2:', solve(40))
