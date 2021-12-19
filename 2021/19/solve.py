a06478443d3a5c26f1e505f8603bdf3d2af1160a
Carl Sandrock <carl.sandrock@proquoai.com>
2021-12-19 15:15:00 +0200

    Simplify and optimise

--- 2021/19/solve.py
+++ 2021/19/solve.py
@@ -1,9 +1,8 @@
 import itertools
-import networkx as nx
 
 import numpy as np
 
-from aoc import solution, print_board
+from aoc import solution
 
 debug = False
 filename = 'test.txt' if debug else 'input.txt'
@@ -32,98 +31,47 @@ def rotate(scanner, rot):
     return scanner[:, order] * np.array(signs)
 
 
-def inverse(rot, delta):
-    """Note: this is broken, it's supposed to find the opposite transform"""
-    order, signs = rot
-    asigns = np.array(signs)
-    aorder = np.array(order)
-    new_order = order
-    new_signs = tuple(asigns[aorder])
-    new_delta = (-delta*asigns)[aorder]
-
-    return (new_order, new_signs), new_delta
-
-
 def rotations(scanner):
+    # We could go twice as fast if we didn't generate 48 rotatoins instead of 24
     for order in itertools.permutations(range(0, 3), 3):
         for signs in itertools.product([-1, 1], repeat=3):
             rot = order, signs
-            yield rotate(scanner, rot), rot
+            yield rotate(scanner, rot)
 
 
 def unique_points(scanner):
     return set(tuple(point) for point in scanner)
 
 
-def overlaps(a, bb):
-    apoints = unique_points(a)
-    for b, rot in rotations(bb):
-        for apoint in a:
+def overlaps(apoints, bb):
+    for b in rotations(bb):
+        for apoint in apoints:
             for bpoint in b:
                 delta = apoint - bpoint
-                common_points = apoints.intersection(unique_points(b + delta))
+                upoints = unique_points(b + delta)
+                common_points = apoints.intersection(upoints)
 
                 if len(common_points) >= 12:
-                    return True, rot, delta
+                    return True, delta, upoints
 
     return False, None, None
 
 
-transforms = {}
-graph = nx.DiGraph()
-
-
-def add_link(scanner_a, scanner_b, rot, delta):
-    graph.add_edge(scanner_a, scanner_b)
-    transforms[(scanner_a, scanner_b)] = rot, delta
-    print(f'Scanner {scanner_a} overlaps Scanner {scanner_b} with {rot=} and {delta=}')
-
-
-for scanner_a in range(len(scanners)):
-    for scanner_b in range(scanner_a + 1, len(scanners)):
-
-        does_overlap, rot, delta = overlaps(scanners[scanner_a], scanners[scanner_b])
-        if not does_overlap:
-            continue
-
-        # figure out reversed transform by brute force because my inverse
-        # formula is broken
-        do_r, rot_r, delta_r = overlaps(scanners[scanner_b], scanners[scanner_a])
-        # print('Inverse found:', rot_r, delta_r)
-        # print('Inverse calc:', *inverse(rot, delta))
-        assert do_r
-
-        add_link(scanner_a, scanner_b, rot, delta)
-        add_link(scanner_b, scanner_a, rot_r, delta_r)
-
-
-transformed_scanners = []
-transformed_locations = []
-for perspective, scanner in enumerate(scanners):
-    path = nx.shortest_path(graph, perspective, 0)
-    transformed_scanner = scanner
-    location = np.zeros((1, 3))
-    for a, b in zip(path, path[1:]):
-        rot, delta = transforms[(b, a)]
-        transformed_scanner = rotate(transformed_scanner, rot) + delta
-        location = rotate(location, rot) + delta
-
-    transformed_scanners.append(transformed_scanner)
-    transformed_locations.append(location[0])
-
-allpoints = set()
-for scanner in transformed_scanners:
-    allpoints.update(unique_points(scanner))
+allpoints = unique_points(scanners.pop(0))
+deltas = []
+while scanners:
+    print('Scanners to go:', len(scanners))
+    scanner = scanners.pop(0)
+    does_overlap, delta, upoints = overlaps(allpoints, scanner)
+    if not does_overlap:
+        scanners.append(scanner)
+        continue
+    allpoints.update(upoints)
+    deltas.append(delta)
 
 # Part 1
 solution(len(allpoints))
 
 # Part 2
-maxdistance = 0
-for i in range(len(transformed_locations)):
-    for j in range(i+1, len(transformed_locations)):
-        distance = np.abs(transformed_locations[i] - transformed_locations[j]).sum()
-        if distance > maxdistance:
-            maxdistance = distance
-
-solution(maxdistance)
+maxdistance = max(np.abs(a - b).sum() for a, b in itertools.product(deltas, repeat=2))
+solution(int(maxdistance))
