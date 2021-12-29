9e8e1dd85e6ac8338b6ac065f082e1f6a38d3051
Carl Sandrock <carl.sandrock@proquoai.com>
2021-12-29 09:42:53 +0200

    Optimize instructions

--- 2021/24/solve.py
+++ 2021/24/solve.py
@@ -5,9 +5,6 @@ from aoc import solution
 
 VALID_DIGITS = list(range(1, 10))
 
-def get_instructions(instruction_str: str):
-    return [line.strip().split() for line in instruction_str.splitlines() if line.strip()]
-
 def parse(filename):
     with open(filename) as f:
         instruction_str = f.read()
@@ -34,15 +31,57 @@ def process(instructions, inputs, invar=None):
             case 'mul', a, b:
                 var[a] *= value(b)
             case 'div', a, b:
-                if value(b) == 0:
-                    raise Exception("Bad div = b=0")
+                # if value(b) == 0:
+                #     raise Exception("Bad div = b=0")
                 var[a] //= value(b)
             case 'mod', a, b:
                 var[a] %= value(b)
             case 'eql', a, b:
                 var[a] = int(var[a] == value(b))
+            # Extended syntax:
+            case 'set', a, b:
+                var[a] = value(b)
+            case 'neq', a, b:
+                var[a] = int(var[a] != value(b))
 
     return var
+
+def optimize(instructions):
+    result = []
+    previous_instruction = None
+    maybe_set = False
+    maybe_neq = False
+    for instruction in instructions:
+        match instruction:
+            case 'div', _, '1':
+                maybe_set = False
+                maybe_neq = False
+            case 'mul', v, '0':
+                maybe_set = v
+                maybe_neq = False
+            case 'add', a, b:
+                if maybe_set == a:
+                    result.append(('set', a, b))
+                else:
+                    # result.append(previous_instruction)
+                    result.append(instruction)
+                maybe_set = False
+            case 'eql', a, '0':
+                if maybe_neq and maybe_neq[0] == a:
+                    result.append(('neq', *maybe_neq))
+                else:
+                    result.append(instruction)
+                maybe_neq = False
+            case 'eql', a, b:
+                maybe_neq = (a, b)
+            case _:
+                if maybe_set or maybe_neq:
+                    result.append(previous_instruction)
+                maybe_set = False
+                maybe_neq = False
+                result.append(instruction)
+        previous_instruction = instruction
+    return tuple(result)
 
 @cache
 def cached_process(part, digit, z):
@@ -129,11 +168,16 @@ def make_serial(parts, z, digits):
 if __name__ == "__main__":
     all_results_cache = pathlib.Path('all_results.pickle')
     instruction_str = parse('input.txt')
-    instructions = get_instructions(instruction_str)
 
     parts = inp_parts(instruction_str)
 
     verify_only_z(parts)
+
+    parts = tuple(optimize(part) for part in parts)
+    # for part in parts:
+    #     for line in part:
+    #         print(' '.join(line))
+    # exit()
 
     CACHE = {}
 
