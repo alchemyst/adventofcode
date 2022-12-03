79655a357501c5dadda8fafb51e651e3596fb23f
Carl Sandrock <carl.sandrock@proquoai.com>
2022-12-03 08:59:49 +0200

    Clean up

--- 2022/03/solve.py
+++ 2022/03/solve.py
@@ -1,3 +1,6 @@
+import functools
+import string
+
 from more_itertools import chunked
 
 from aoc import solution
@@ -6,36 +9,41 @@ debug = False
 filename = 'test.txt' if debug else 'input.txt'
 
 with open(filename) as f:
-    lines = f.readlines()
+    lines = f.read().splitlines()
 
-priority = {chr(ord('a') + i): i+1 for i in range(26)}
-priority |= {chr(ord('A') + i): i+27 for i in range(26)}
-
+priority = {
+    c: score for score, c in
+    enumerate(string.ascii_lowercase + string.ascii_uppercase, 1)
+}
 
 def score1(line):
-    line = line.strip()
     half = len(line)//2
-    compartment1, compartment2 = line[:half], line[half:]
+    parts = map(set, [line[:half], line[half:]])
 
-    common, = set(compartment1).intersection(compartment2)
+    common, = set.intersection(*parts)
+
     if debug:
         print(common)
-    return priority[common]
+
+    return common
+
+
+def run(f, items):
+    return sum(priority[f(item)] for item in items)
 
 
 # Part 1
-solution(sum(score1(line) for line in lines))
+solution(run(score1, lines))
 
 
 def score2(group):
-    a, b, c = [set(line.strip()) for line in group]
-    common, = a.intersection(b).intersection(c)
+    common, = functools.reduce(set.intersection, map(set, group))
 
     if debug:
         print(common)
 
-    return priority[common]
+    return common
 
 
 # Part 2
-solution(sum(score2(group) for group in chunked(lines, 3)))
+solution(run(score2, chunked(lines, 3)))
