d45447db8d021c1811ac62b03243819a55dc8ce6
Carl Sandrock <carl.sandrock@proquoai.com>
2022-12-09 08:11:40 +0200

    Refactor

--- 2022/09/solve.py
+++ 2022/09/solve.py
@@ -1,4 +1,4 @@
-from aoc import solution, print_board
+from aoc import solution
 import numpy as np
 
 debug = False
@@ -11,75 +11,40 @@ vectors = {
     'R': 1,
 }
 
-instructions = []
-with open(filename) as f:
-    for line in f:
-        direction, step_str = line.strip().split()
-        steps = int(step_str)
+def read_instructions(filename):
+    instructions = []
+    with open(filename) as f:
+        for line in f:
+            direction, step_str = line.strip().split()
+            steps = int(step_str)
 
-        instructions.append((direction, steps))
+            instructions.append((direction, steps))
 
-# def board(head, tail):
-#     size = 6
-#     b = np.zeros((size, size), dtype=str)
-#     b[:, :] = '.'
-#
-#     b[int(size-tail.imag-1), int(tail.real)] = 'T'
-#     b[int(size-head.imag-1), int(head.real)] = 'H'
-#
-#     return b
+    return instructions
 
-head = 0 + 0j
-tail = head
-tail_history = {tail}
 
-# if debug:
-#     print_board(board(head, tail), type='s')
-#     print()
-for direction, steps in instructions:
-    for _ in range(steps):
-        head += vectors[direction]
-        taildiff = head - tail
+def simulate(instructions, length):
+    rope = [complex()]*length
+    tail_history = {rope[-1]}
 
-        if abs(taildiff.real) > 1 or abs(taildiff.imag) > 1:  # not adjacent
-            tailstep = np.sign(taildiff.imag)*1j + np.sign(taildiff.real)
-        else:
-            tailstep = 0
+    for direction, steps in instructions:
+        for _ in range(steps):
+            rope[0] += vectors[direction]
+            for i in range(1, len(rope)):
+                diff = rope[i-1] - rope[i]
 
-        tail += tailstep
+                if abs(diff) > 1:
+                    step = np.sign(diff.imag)*1j + np.sign(diff.real)
+                    rope[i] += step
 
-        # if debug:
-        #     print_board(board(head, tail), type='s')
-        #     print()
-        tail_history.add(tail)
+            tail_history.add(rope[i])
 
+    return len(tail_history)
+
+instructions = read_instructions(filename)
 
 # Part 1
-solution(len(tail_history))
-
-rope = [0 + 0j]*10
-tail_history = {0 + 0j}
-
-# if debug:
-#     print_board(board(head, tail), type='s')
-#     print()
-for direction, steps in instructions:
-    for _ in range(steps):
-        rope[0] += vectors[direction]
-        for i in range(1, len(rope)):
-            head = rope[i-1]
-            tail = rope[i]
-
-            taildiff = head - tail
-
-            if abs(taildiff.real) > 1 or abs(taildiff.imag) > 1:  # not adjacent
-                tailstep = np.sign(taildiff.imag)*1j + np.sign(taildiff.real)
-            else:
-                tailstep = 0
-
-            rope[i] += tailstep
-
-        tail_history.add(rope[i])
+solution(simulate(instructions, 2))
 
 # Part 2
-solution(len(tail_history))
+solution(simulate(instructions, 10))
