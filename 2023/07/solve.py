af82a530fc8c205c13393982950bf57565432d7d
Carl Sandrock <carl.sandrock@proquoai.com>
2023-12-07 08:42:20 +0200

    Golf 2023/07 a bit

--- 2023/07/solve.py
+++ 2023/07/solve.py
@@ -1,9 +1,19 @@
 from aoc import solution
 from collections import Counter
 from functools import cache, partial
+import parse
 
 debug = False
 filename = 'test.txt' if debug else 'input.txt'
+
+# 6 Five of a kind, where all five cards have the same label: AAAAA
+# 5 Four of a kind, where four cards have the same label and one card has a different label: AA8AA
+# 4 Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
+# 3 Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
+# 2 Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
+# 1 One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
+# 0 High card, where all cards' labels are distinct: 23456
+HAND_VALUES = ((1, 1, 1, 1, 1), (1, 1, 1, 2), (1, 2, 2), (1, 1, 3), (2, 3), (1, 4), (5,))
 
 
 def sort_hand(hand):
@@ -12,31 +22,20 @@ def sort_hand(hand):
 
 @cache
 def kind(cards, joker, hand):
-    # 6 Five of a kind, where all five cards have the same label: AAAAA
-    # 5 Four of a kind, where four cards have the same label and one card has a different label: AA8AA
-    # 4 Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
-    # 3 Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
-    # 2 Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
-    # 1 One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
-    # 0 High card, where all cards' labels are distinct: 23456
-    counter = Counter(hand)
-    counts = counter.values()
-    if 5 in counts:
+    counts = Counter(hand).values()
+
+    if counts == (5,):
         return 6  # Five of a kind even if jokers
 
-    if joker and 'J' in hand:
-        return max(
+    return (
+        max(
             kind(cards, joker, sort_hand(hand.replace('J', card)))
-            for card in cards if card != 'J'
+            for card in cards
+            if card != 'J'
         )
-    elif 4 in counts:
-        return 5  # Four of a kind
-    elif 3 in counts:
-        return 4 if 2 in counts else 3
-    elif 2 in counts:
-        return 2 if len(counter) == 3 else 1
-    else:
-        return 0  # High card
+        if joker and 'J' in hand
+        else HAND_VALUES.index(tuple(sorted(counts)))
+    )
 
 
 @cache
@@ -50,19 +49,16 @@ def keyfunc(cards, joker, hand_bid):
     return kind(cards, joker, sort_hand(hand)), card_strengths(cards, hand)
 
 
-hands = []
-with open(filename) as f:
-    for line in f:
-        hand, bid = line.strip().split(' ')
-        hands.append((hand, int(bid)))
-
-
 def solve(cards, joker):
     local_keyfunc = partial(keyfunc, cards, joker)
     return sum(
         rank * bid
         for rank, (hand, bid) in enumerate(sorted(hands, key=local_keyfunc), 1)
     )
+
+
+parser = parse.compile('{} {:d}')
+hands = [parser.parse(line.strip()) for line in open(filename)]
 
 
 # Part 1
