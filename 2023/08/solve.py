768af525efe61719b4974d1b50d4afd569fba060
Carl Sandrock <carl.sandrock@proquoai.com>
2023-12-08 11:01:13 +0200

    Clean up and simplify

--- 2023/08/solve.py
+++ 2023/08/solve.py
@@ -1,5 +1,4 @@
 import math
-from functools import cache
 
 from aoc import solution
 import parse
@@ -36,52 +35,26 @@ solution(n)
 
 # Part 2
 filename = 'test2.txt' if debug else 'input.txt'
-
 nodes, lr_instructions = read_nodes(filename)
 
-state_cache = {}
 
-def steps_to_next_z(node, instruction):
-    key = (node, instruction)
-    if key in state_cache:
-        return state_cache[key]
-
-    keys = [key]
-    steps = 0
-    while True:
-        node = nodes[node][lr_instructions[instruction]]
-        instruction = (instruction + 1) % len(lr_instructions)
-        steps += 1
-        key = (node, instruction)
-        if key in state_cache:
-            # fast-forward to solution state
-            node, instruction, extra_steps = state_cache[key]
-            steps += extra_steps
+def steps_to_next_z(node, instructions):
+    for steps, instruction in enumerate(instructions, 1):
+        node = nodes[node][instruction]
 
         if node.endswith("Z"):
-            solution = node, instruction, steps
-            for i, visited_key in enumerate(keys):
-                state_cache[visited_key] = node, instruction, steps - i
-            return solution
-        keys.append(key)
+            return node, instructions, steps
 
 
-states = [(n, 0, 0) for n in nodes if n.endswith('A')]
+states = [(n, cycle(lr_instructions), 0) for n in nodes if n.endswith('A')]
 
-for i in range(10):
-    steps = [state[2] for state in states]
-    max_steps = max(steps)
-    if max_steps > 0 and all(step == max_steps for step in steps):
-        break
-
+# settle into a cycle
+for _ in range(3):
     for position, (node, instruction, steps) in enumerate(states):
-        if max_steps > 0 and steps == max_steps:
-            continue
         node, instruction, extra_steps = steps_to_next_z(node, instruction)
         states[position] = (node, instruction, steps + extra_steps)
 
 cycle_times = []
-
 for node, instruction, steps in states:
     _, _, cycle_time = steps_to_next_z(node, instruction)
     cycle_times.append(cycle_time)
