d5f4a6833582d921f31ec3a1d7b1855bf621c0c1
Carl Sandrock <carl.sandrock@proquoai.com>
2023-12-11 07:46:59 +0200

    Refactor and simplify 2023/11

--- 2023/11/solve.py
+++ 2023/11/solve.py
@@ -6,35 +6,24 @@ import scipy.spatial as spatial
 debug = False
 filename = 'test.txt' if debug else 'input.txt'
 
-def expand(board):
-    rows = []
-    for row in board:
-        rows.append(row)
-        if all(row == '.'):
-            rows.append(row)
-    return np.array(rows)
-
-
 board = np.array(read_board(filename))
-expanded = expand(expand(board.T).T)
-nodes = np.array((expanded == '#').nonzero()).T
-distances = spatial.distance_matrix(nodes, nodes, p=1)
+
+
+def solve(factor):
+    nodes = np.array((board == '#').nonzero()).T
+
+    for axis in (0, 1):
+        expansion = (board == '.').all(axis=axis).cumsum()*factor
+        coords = nodes[:, 1-axis]
+        coords += expansion[coords]
+
+    distances = spatial.distance_matrix(nodes, nodes, p=1)
+    return int(np.tril(distances).sum())
+
 
 # Part 1
-solution(np.tril(distances).sum().astype(int))
+solution(solve(1))
 
 # Part 2
-factor = 1000000 - 1
-nodes = np.array((board == '#').nonzero()).T
-# expand out rows:
-row_expansion = (board == '.').all(axis=1).cumsum()*factor
-column_expansion = (board == '.').all(axis=0).cumsum()*factor
-row_coords = nodes[:, 0]
-column_coords = nodes[:, 1]
 
-nodes[:, 0] += row_expansion[row_coords]
-nodes[:, 1] += column_expansion[column_coords]
-
-distances = spatial.distance_matrix(nodes, nodes, p=1)
-
-solution(np.tril(distances).sum())
+solution(solve(1000000 - 1))
