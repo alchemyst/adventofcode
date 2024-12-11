53b40f9380f7c05a349a3c4e5a1c5646a0de611f
Carl Sandrock <carl.sandrock@proquoai.com>
2024-12-11 08:01:49 +0200

    Simplify

--- 2024/11/solve.py
+++ 2024/11/solve.py
@@ -10,39 +10,6 @@ filename = 'test.txt' if debug else 'input.txt'
 def read_stones():
     return aoc.parse.all_numbers(pathlib.Path(filename).read_text())
 
-stones = read_stones()
-
-# Part 1
-def apply_rule(stones):
-    new_stones = []
-    for stone in stones:
-        if stone == 0:
-            new_stones.append(1)
-            continue
-
-        digits = f"{stone}"
-        if len(digits) % 2 == 0:
-            midway = len(digits) // 2
-            new_stones += [int(digits[:midway]), int(digits[midway:])]
-            continue
-
-        new_stones.append(stone*2024)
-    return new_stones
-
-if debug:
-    print("Initial arrangement:")
-    print(stones)
-
-for i in range(25):
-    # print(i)
-    stones = apply_rule(stones)
-    if debug:
-        print(f"After {i+1} blinks")
-        print(stones[0])
-
-solution(len(stones))
-
-# Part 2
 @lru_cache(maxsize=100000)
 def children(stone, turns):
     if turns == 0:
@@ -58,5 +25,13 @@ def children(stone, turns):
 
     return children(stone*2024, turns - 1)
 
+def nstones(stones, turns):
+    return sum(children(stone, turns) for stone in stones)
+
+# Part 1
 stones = read_stones()
-solution(sum(children(stone, 75) for stone in stones))
+solution(nstones(stones, 25))
+
+# Part 2
+stones = read_stones()
+solution(nstones(stones, 75))
