69cad266e6b2e463510fdd423fb97a889eff234a
Carl Sandrock <carl.sandrock@proquoai.com>
2024-12-23 14:59:21 +0200

    2024/21 Part 2 solve

--- 2024/21/solve.py
+++ 2024/21/solve.py
@@ -1,9 +1,9 @@
 import itertools
-from dataclasses import dataclass
+from collections import defaultdict
+from functools import lru_cache
 from itertools import pairwise
 from textwrap import dedent
 import networkx as nx
-from functools import lru_cache
 
 from aoc import solution
 
@@ -13,143 +13,79 @@ filename = 'test.txt' if debug else 'input.txt'
 with open(filename) as f:
     lines = f.read().splitlines(keepends=False)
 
-@dataclass
-class Finger:
-    moves: dict[tuple[str, str], str]
-    position: str = 'A'
-
-    def simulate(self, moves, start='A'):
-        output = ''
-        self.position = start
-        for move in moves:
-            if move == 'A':
-                output += self.position
-                continue
-
-            key = (self.position, move)
-            if key not in self.moves:
-                raise ValueError(f'Invalid move: {key}')
-
-            self.position = self.moves[key]
-
-        return output
-
 directions = {
     '^': (-1, 0),
     'v': (1, 0),
     '<': (0, -1),
     '>': (0, 1)
 }
-def keypad_to_moves(keypad: str):
-    lines = keypad.strip("\n").split('\n')
-    moves = {}
+
+def graph_from_keypad(keypad: str):
+    lines = dedent(keypad).strip("\n").split('\n')
+
+    graph = nx.DiGraph()
     for i, row in enumerate(lines):
         for j, key in enumerate(row):
             if key == ' ':
                 continue
             for direction, delta in directions.items():
-                new_i, new_j = i + delta[0], j + delta[1]
-                if new_i < 0 or new_i >= len(lines) or new_j < 0 or new_j >= len(row) or lines[new_i][new_j] == ' ':
-                    continue
-                moves[(key, direction)] = lines[new_i][new_j]
-    return moves
+                other_i, other_j = i + delta[0], j + delta[1]
+                if 0 <= other_i < len(lines) and 0 <= other_j < len(row):
+                    other_key = lines[other_i][other_j]
+                    if other_key == ' ':
+                        continue
 
+                    graph.add_edge(key, other_key, direction=direction)
 
-numeric = Finger(keypad_to_moves(dedent(
+    return graph
+
+numeric = graph_from_keypad(
     """
     789
     456
     123
      0A
     """
-)))
+)
 
-directional_moves = keypad_to_moves(dedent(
+directional = graph_from_keypad(
     """
      ^A
     <v>
     """
-))
+)
 
-@dataclass
-class Stack:
-    fingers: list[Finger]
+moves = defaultdict(list)
 
-    def simulate(self, moves, starts):
-        state = ''
-        for finger, start in zip(self.fingers, starts):
-            output = finger.simulate(moves, start)
-            state += finger.position
-            moves = output
-
-        return output, state
-
-# Part 1
-stack = Stack([Finger(directional_moves), Finger(directional_moves), numeric])
-# output = print(stack.simulate("<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A", 'AAA'))
-
-pad_types = [numeric, Finger(directional_moves)]
+for i, graph in enumerate([numeric, directional]):
+    for start, end in itertools.product(graph.nodes, repeat=2):
+        for path in nx.all_shortest_paths(graph, start, end):
+            move = ''.join(graph[path_a][path_b]["direction"] for path_a, path_b in pairwise(path)) + 'A'
+            moves[start, end].append(move)
+        if start == end:
+            moves[start, end] = ["A"]
 
 @lru_cache
-def graph_from_finger(finger):
-    graph = nx.DiGraph()
-    for (start, move), end in pad_types[finger].moves.items():
-        graph.add_edge(start, end, move=move)
-
-    return graph
-
-@lru_cache(maxsize=1000000)
-def all_shortest_paths(finger, start, end):
-    graph = graph_from_finger(finger)
-    options = []
-    for path in nx.all_shortest_paths(graph, start, end):
-        output = (
-            ''.join(
-                graph[node1][node2]['move'] for node1, node2 in pairwise(path)
+def shortest_path(code, depth):
+    length = 0
+    for start, end in pairwise("A" + code):
+        if depth == 0:
+            length += len(moves[start, end][0])
+        else:
+            length += min(
+                shortest_path(move, depth - 1) for move in moves[start, end]
             )
-            + "A"
-        )
-        options.append(output)
 
-    return options
+    return length
 
 
-@lru_cache(maxsize=1000000)
-def shortest(sequence, finger):
-
-    outputs = []
-    for start, end in pairwise("A" + sequence):
-        outputs.append(all_shortest_paths(finger, start, end))
-
-    return [''.join(combo) for combo in itertools.product(*outputs)]
-
-
-def solve(stack_size):
-    stack = [0] + [1]*stack_size
-    s = 0
-    for code in lines:
-        sequences = [code.strip()]
-
-        for finger in stack:
-            new_sequences = set()
-            for sequence in sequences:
-                subsequences = [s + "A" for s in sequence.split("A")[:-1]]
-
-                new_sequences.update(''.join(combo) for combo in itertools.product(*[shortest(subsequence, finger) for subsequence in subsequences]))
-
-
-            min_len = min(len(s) for s in new_sequences)
-            sequences = [s for s in new_sequences if len(s) == min_len]
-            print(len(sequences))
-
-        sequence = min(sequences, key=lambda x: len(x))
-        complexity = len(sequence) * int(code[:-1])
-
-        s += complexity
-
-    return s
+def solve(depth):
+    return sum(
+        shortest_path(code, depth)*int(code[:3])
+        for code in lines
+    )
 
 solution(solve(2))
 
 # Part 2
-solution('Dummy')
+solution(solve(25))
